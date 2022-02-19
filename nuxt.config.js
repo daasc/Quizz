3d05bb75ba7e361418019cdb28920c078325bbe4
daasc <paulojhol@gmail.com>
2022-02-19 18:27:41 -0300

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    // SCSS file in the project
+    '@/assets/css/main.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Hello</h1>
+  </div>
 </template>
 
 <script>
